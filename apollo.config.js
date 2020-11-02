d797c8aa1753314934a4fecd9fb2f2f62e0ede13
Divya Prakash <medivyaprakash@gmail.com>
2020-11-03 00:04:47 +0530

    update

--- apollo.config.js
+++ apollo.config.js
@@ -1,12 +1,20 @@
 module.exports = {
-  client: {
-    addTypename: false,
-    excludes: [],
-    includes: ["./src/**/*.tsx","./src/**/*.ts","./plugins/**/*.js","./node_modules/gatsby-source-contentful/src/*.js","./node_modules/gatsby-source-datocms/fragments/*.js","./node_modules/gatsby-transformer-sharp/src/*.js","./node_modules/gatsby-image/src/*.js"],
-    service: {
-      name: "gatsbySchema",
-      localSchemaFile: "./schema.json"
+    client: {
+        addTypename: false,
+        excludes: [],
+        includes: [
+            "./src/**/*.tsx",
+            "./src/**/*.ts",
+            "./plugins/**/*.js",
+            "./node_modules/gatsby-source-contentful/src/*.js",
+            "./node_modules/gatsby-source-datocms/fragments/*.js",
+            "./node_modules/gatsby-transformer-sharp/src/*.js",
+            "./node_modules/gatsby-image/src/*.js",
+        ],
+        service: {
+            name: "gatsbySchema",
+            localSchemaFile: "./schema.json",
+        },
+        tagName: "graphql",
     },
-    tagName: "graphql"
-  }
 }
--- changelog.md
+++ changelog.md
@@ -1,21 +1,20 @@
 # v2.1.2
 
-- Reload disqus on theme switch
+-   Reload disqus on theme switch
 
 # v2.1.1
 
-- Cookie Policy
-- Fix ogIMage
+-   Cookie Policy
+-   Fix ogIMage
 
 # v2.1.0
 
-- Typescript
-
+-   Typescript
 
 # v2.0.0
 
-- Switched from less to tailwindcss
-- More customization on front page such as two column layout
-- Improved mobile responsiveness
-- Highly configurable contact form
-- Better theme switching
+-   Switched from less to tailwindcss
+-   More customization on front page such as two column layout
+-   Improved mobile responsiveness
+-   Highly configurable contact form
+-   Better theme switching
